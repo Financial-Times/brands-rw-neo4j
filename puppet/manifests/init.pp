fa9dce1856fb738f65c83311677a99d4e2bb7328
Dan Murphy <dan.murphy@ft.com>
2016-01-21 15:23:53 +0000

    correct configParams assignment err

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -1,8 +1,10 @@
 class brands-rw-neo4j {
 
+  $configParameters = hiera('configParameters','')
+  
   class { "go_service_profile" :
     service_name => "brands-rw-neo4j"
-    configParameters => hiera('configParameters','')
+    configParameters => "$configParameters"
   }
-  
+
 }
