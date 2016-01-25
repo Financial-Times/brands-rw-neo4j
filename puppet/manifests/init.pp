73b7a0e032bc6bdcdf471389ec62219989b9017c
Dan Murphy <dan.murphy@ft.com>
2016-01-25 15:19:55 +0000

    Pass in my module name to dependencies

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -3,6 +3,7 @@ class brands_rw_neo4j {
   $configParameters = hiera('configParameters','')
 
   class { "go_service_profile" :
+    service_module => $module_name
     service_name => 'brands-rw-neo4j',
     configParameters => $configParameters
   }
