55521a6b70d657be405940ccb49c1eb3847eb0ef
Dan Murphy <dan.murphy@ft.com>
2016-02-15 09:43:59 +0000

    Set omitempty on ParentUUID

--- brands/model.go
+++ brands/model.go
@@ -5,7 +5,7 @@ type Brand struct {
 	UUID           string `json:"uuid"`
 	PrefLabel      string `json:"prefLabel"`
 	Description    string `json:"description"`
-	ParentUUID     string `json:"parentUUID"`
+	ParentUUID     string `json:"parentUUID,omitempty"`
 	Strapline      string `json:"strapline"`
 	DescriptionXML string `json:"descriptionXML"`
 	ImageURL       string `json:"_imageUrl"` // TODO this is a temporary thing - needs to be integrated into images properly
